4f8a17f5a4131bcaccc86d2bf5171975095bf94d
sowmiya <csowmiya204@gmail.com>
2026-04-26 13:29:38 +0530

    update test

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function mark(btn) {
-    btn.innerHTML = "Present ✔";
-    btn.style.backgroundColor = "green";
-    btn.style.color = "white";
+function markPresent() {
+    let name = document.getElementById("name").value;
+    if(name === "") {
+        alert("Enter student name");
+        return;
+    }
+    document.getElementById("status").innerHTML = name + " is Present ✔";
+    document.getElementById("status").style.color = "green";
+}
+
+function markAbsent() {
+    let name = document.getElementById("name").value;
+    if(name === "") {
+        alert("Enter student name");
+        return;
+    }
+    document.getElementById("status").innerHTML = name + " is Absent ❌";
+    document.getElementById("status").style.color = "red";
 }
